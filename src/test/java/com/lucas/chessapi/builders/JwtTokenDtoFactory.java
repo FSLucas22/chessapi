0a414fb0ca0c164f04392b0ffcb1312071738281
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-07 08:59:17 -0300

    refactor: Simplified class

--- src/test/java/com/lucas/chessapi/builders/JwtTokenDtoFactory.java
+++ src/test/java/com/lucas/chessapi/builders/JwtTokenDtoFactory.java
@@ -2,15 +2,16 @@ package com.lucas.chessapi.builders;
 
 import com.lucas.chessapi.security.jwt.JwtTokenDto;
 
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class JwtTokenDtoFactory {
-    private final static Date today = Date.from(Instant.now().truncatedTo(ChronoUnit.DAYS));
-    private final static Date tomorrow = Date.from(today.toInstant().plus(1, ChronoUnit.DAYS));
 
     public static JwtTokenDto jwtTokenDtoWithSubject(String subject) {
-        return new JwtTokenDto(subject, today, tomorrow);
+        return new JwtTokenDto(subject, DateFactory.today(), tomorrow());
+    }
+
+    private static Date tomorrow() {
+        return Date.from(DateFactory.today().toInstant().plus(1, ChronoUnit.DAYS));
     }
 }
