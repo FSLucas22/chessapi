a68eb22b0fbf649e75bbdfaa41083057be6a55ed
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-13 00:24:54 -0300

    refactor(GetUserServiceTest): Simplify tests removing inheritance

--- src/test/java/com/lucas/chessapi/unit/service/GetUserServiceTest.java
+++ src/test/java/com/lucas/chessapi/unit/service/GetUserServiceTest.java
@@ -1,58 +1,88 @@
 package com.lucas.chessapi.unit.service;
 
-import com.lucas.chessapi.builders.UserEntityBuilderExtension;
-import com.lucas.chessapi.domain.service.ContextGetUserServiceTest;
 import com.lucas.chessapi.dto.request.GetAllUsersRequestDto;
 import com.lucas.chessapi.dto.response.GetAllUsersResponseDto;
 import com.lucas.chessapi.dto.response.GetUserResponseDto;
 import com.lucas.chessapi.exceptions.GetUserException;
-import com.lucas.chessapi.model.UserEntity;
+import com.lucas.chessapi.repository.UserRepository;
+import com.lucas.chessapi.service.impl.GetUserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.Optional;
 
+import static com.lucas.chessapi.builders.UserEntityBuilderExtension.user;
 import static com.lucas.chessapi.builders.UserEntityBuilderExtension.validUserEntityWithId;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
-public class GetUserServiceTest extends ContextGetUserServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class GetUserServiceTest {
+    @Mock
+    private UserRepository repository;
+    @InjectMocks
+    private GetUserServiceImpl service;
 
     @Test
     void shouldReturnUserInformationById() {
+        // GIVEN
         var user = validUserEntityWithId(1L);
-        givenUserExists(user);
-        whenGetByIdIsCalledWith(1L);
-        thenShouldHaveNoErrors();
-        thenResultShouldBe(new GetUserResponseDto(user.getId(), user.getUsername(), user.getEmail()));
+        when(repository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        // WHEN
+        GetUserResponseDto result = service.getById(1L);
+
+        // THEN
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(new GetUserResponseDto(user.getId(), user.getUsername(), user.getEmail()));
     }
 
     @Test
     void shouldThrowGetUserExceptionWhenUserDontExist() {
-        whenGetByIdIsCalledWith(1L);
-        thenShouldThrow(GetUserException.class, "User not found");
+        // WHEN
+        assertThatThrownBy(() -> service.getById(1L))
+                // THEN
+                .isInstanceOf(GetUserException.class)
+                .hasMessage("User not found");
     }
 
     @Test
     void shouldGetAllUsersInPaginationSortedByName() {
+        // GIVEN
         var request = new GetAllUsersRequestDto(0, 3, List.of("username"));
-        givenUserListIsReturned(List.of(
+        var users = List.of(
                 user(3L, "Alice"),
                 user(2L, "Bob"),
                 user(1L, "Cheryl")
-        ));
-        whenGetAllIsCalledWith(request);
-        thenShouldHaveNoErrors();
-        thenResultShouldBe(new GetAllUsersResponseDto(List.of(
-                new GetUserResponseDto(3L, "Alice", "alice@email.com"),
-                new GetUserResponseDto(2L, "Bob", "bob@email.com"),
-                new GetUserResponseDto(1L, "Cheryl", "cheryl@email.com")
-        )));
-    }
+        );
 
-    private UserEntity user(Long id, String username) {
-        return UserEntityBuilderExtension
-                .valid()
-                .id(id)
-                .username(username)
-                .email(username.toLowerCase() + "@email.com")
-                .build();
+        var pageable = PageRequest.of(
+                0,
+                users.size(),
+                Sort.by(Sort.Order.asc("username"))
+        );
+        when(repository.findAll(pageable))
+                .thenReturn(new PageImpl<>(users, pageable, users.size()));
+
+        // WHEN
+        var result = service.getAll(request);
+
+        // THEN
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(new GetAllUsersResponseDto(List.of(
+                        new GetUserResponseDto(3L, "Alice", "alice@email.com"),
+                        new GetUserResponseDto(2L, "Bob", "bob@email.com"),
+                        new GetUserResponseDto(1L, "Cheryl", "cheryl@email.com")
+                )));
     }
 }
