75feebbfa3e1dbd535f3ac94ab972fb6eb045763
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-13 00:56:21 -0300

    refactor(GetGameServiceTest): Simplify tests removing inheritance

--- src/test/java/com/lucas/chessapi/unit/service/GetGameServiceTest.java
+++ src/test/java/com/lucas/chessapi/unit/service/GetGameServiceTest.java
@@ -2,40 +2,73 @@ package com.lucas.chessapi.unit.service;
 
 import com.lucas.chessapi.builders.GameEntityBuilder;
 import com.lucas.chessapi.builders.UserEntityBuilder;
-import com.lucas.chessapi.domain.service.ContextGetGameServiceTest;
 import com.lucas.chessapi.dto.game.PlayerDto;
 import com.lucas.chessapi.dto.response.GetAllGamesResponseDto;
 import com.lucas.chessapi.dto.response.GetGameResponseDto;
+import com.lucas.chessapi.dto.response.GetUserResponseDto;
 import com.lucas.chessapi.exceptions.GameNotFoundException;
 import com.lucas.chessapi.exceptions.GetUserException;
 import com.lucas.chessapi.exceptions.PlayerNotFoundException;
+import com.lucas.chessapi.model.GameEntity;
+import com.lucas.chessapi.model.UserEntity;
+import com.lucas.chessapi.repository.GameRepository;
+import com.lucas.chessapi.service.GetUserService;
+import com.lucas.chessapi.service.impl.GetGameServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-public class GetGameServiceTest extends ContextGetGameServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class GetGameServiceTest {
+    @Mock
+    GameRepository repository;
+
+    @Mock
+    GetUserService userService;
+
+    @InjectMocks
+    GetGameServiceImpl service;
 
     @Test
     void shouldReturnGameById() {
         var game = GameEntityBuilder.valid().id(1L).build();
         var firstPlayer = game.getFirstPlayer();
         var secondPlayer = game.getSecondPlayer();
-        given(game);
-        whenGetByIdIsCalledWith(1L);
-        thenShouldHaveNoErrors();
-        thenResultShouldMatch(new GetGameResponseDto(
-                game.getId(),
-                new PlayerDto(firstPlayer.getId(), firstPlayer.getUsername()),
-                new PlayerDto(secondPlayer.getId(), secondPlayer.getUsername()),
-                game.getCreatedAt(),
-                game.getUpdatedAt()
-        ));
+        when(repository.findById(game.getId()))
+                .thenReturn(Optional.of(game));
+
+        var result = service.getById(1L);
+
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(new GetGameResponseDto(
+                        game.getId(),
+                        new PlayerDto(firstPlayer.getId(), firstPlayer.getUsername()),
+                        new PlayerDto(secondPlayer.getId(), secondPlayer.getUsername()),
+                        game.getCreatedAt(),
+                        game.getUpdatedAt()
+                ));
     }
 
     @Test
     void shouldThrowGameNotFoundExceptionWhenGameDontExist() {
-        whenGetByIdIsCalledWith(1L);
-        thenShouldThrow(GameNotFoundException.class, "Game not found");
+        assertThatThrownBy(() -> service.getById(1L))
+                .isInstanceOf(GameNotFoundException.class)
+                .hasMessage("Game not found");
     }
 
     @Test
@@ -46,25 +79,46 @@ public class GetGameServiceTest extends ContextGetGameServiceTest {
 
         given(player);
         givenGamesFromUser(game1, game2);
-        whenGetAllByUserIdIsCalledWith(5L);
-        thenShouldHaveNoErrors();
-        thenAllGamesResultShoudMatch(new GetAllGamesResponseDto(
-                PlayerDto.from(player),
-                List.of(GetGameResponseDto.from(game1), GetGameResponseDto.from(game2)),
-                false
-        ));
+
+        var allGamesResult = service.getAllByUserId(5L, 0, 3);
+
+        assertThat(allGamesResult)
+                .usingRecursiveComparison()
+                .isEqualTo(new GetAllGamesResponseDto(
+                        PlayerDto.from(player),
+                        List.of(GetGameResponseDto.from(game1), GetGameResponseDto.from(game2)),
+                        false
+                ));
     }
 
     @Test
     void shouldThrowPlayerNotFoundExceptionWhenUserDontExist() {
-        givenUserServiceWillThrow(new GetUserException("User not found"));
-        whenGetAllByUserIdIsCalledWith(1L);
-        thenShouldThrow(PlayerNotFoundException.class, "Player not found");
-        thenPlayerNotFoundIdShouldBe(1L);
-        thenShouldNotInteractWithGameRepository();
+        when(userService.getById(any())).thenThrow(new GetUserException("User not found"));
+
+        var exception = assertThrows(
+                PlayerNotFoundException.class,
+                () -> service.getAllByUserId(1L, 0, 3)
+        );
+
+        assertThat(exception).hasMessage("Player not found");
+        assertThat(exception.getPlayerId()).isEqualTo(1L);
+        verify(repository, never()).findAllByUser(any(), any());
+        verify(repository, never()).findById(any());
     }
 
-    private GameEntityBuilder game(Long id) {
+    GameEntityBuilder game(Long id) {
         return GameEntityBuilder.valid().id(id);
+    }
+
+    void given(UserEntity... users) {
+        Arrays.stream(users).forEach(
+                u -> when(userService.getById(u.getId()))
+                        .thenReturn(GetUserResponseDto.from(u))
+        );
+    }
+
+    void givenGamesFromUser(GameEntity... games) {
+        Slice<GameEntity> gameSlice = new SliceImpl<>(List.of(games));
+        when(repository.findAllByUser(any(), any())).thenReturn(gameSlice);
     }
 }
