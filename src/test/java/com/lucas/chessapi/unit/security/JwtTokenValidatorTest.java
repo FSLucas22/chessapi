ce33f3411bdd0d6c27b7ef1e0d7540cc04d90b5f
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-12 22:49:43 -0300

    refactor(JwtTokenValidatorTest): Simplify tests removing inheritance

--- src/test/java/com/lucas/chessapi/unit/security/JwtTokenValidatorTest.java
+++ src/test/java/com/lucas/chessapi/unit/security/JwtTokenValidatorTest.java
@@ -1,38 +1,71 @@
 package com.lucas.chessapi.unit.security;
 
-import com.lucas.chessapi.domain.security.ContextJwtTokenValidatorTest;
 import com.lucas.chessapi.exceptions.InvalidTokenException;
-import io.jsonwebtoken.JwtException;
+import com.lucas.chessapi.security.jwt.JwtTokenValidator;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.MalformedJwtException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.lucas.chessapi.builders.JwtTokenDtoFactory.jwtTokenDtoWithSubject;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
-public class JwtTokenValidatorTest extends ContextJwtTokenValidatorTest {
-    private final String token = "1234";
+@ExtendWith(MockitoExtension.class)
+public class JwtTokenValidatorTest {
+    @Mock
+    Jws<Claims> claimsJws;
+
+    @Mock
+    JwtParser parser;
+
+    @InjectMocks
+    JwtTokenValidator validator;
+
+    final String token = "1234";
 
     @Test
     void shouldDoNothingWhenTokenIsValid() {
+        // GIVEN
         var claims = jwtTokenDtoWithSubject("1").toClaims();
-        givenJwtParserReturnsClaims(claims, token);
-        whenValidationIsMadeFor(token);
-        thenShouldHaveNoErrors();
+        when(parser.parseClaimsJws(token)).thenReturn(claimsJws);
+        when(claimsJws.getBody()).thenReturn(claims);
+
+        // WHEN
+        validator.validate(token);
+
+        // THEN
     }
 
     @Test
     void shouldThrowInvalidTokenExceptionWhenSubjectIsNotLong() {
+        // GIVEN
         var claims = jwtTokenDtoWithSubject("abc").toClaims();
 
-        givenJwtParserReturnsClaims(claims, token);
-        whenValidationIsMadeFor(token);
-        thenShouldThrow(InvalidTokenException.class, "Subject must be a valid Long");
+        when(parser.parseClaimsJws(token)).thenReturn(claimsJws);
+        when(claimsJws.getBody()).thenReturn(claims);
+
+        // WHEN
+        assertThatThrownBy(() -> validator.validate(token))
+                // THEN
+                .isInstanceOf(InvalidTokenException.class)
+                .hasMessage("Subject must be a valid Long");
     }
 
     @Test
     void shouldThrowInvalidTokenExceptionWhenParserThrowsJwtException() {
-        JwtException exception = new MalformedJwtException("test");
-        givenJwtParserThrows(exception, token);
-        whenValidationIsMadeFor(token);
-        thenShouldThrow(InvalidTokenException.class, "test");
+        // GIVEN
+        when(parser.parseClaimsJws(token)).thenThrow(new MalformedJwtException("test"));
+
+        // WHEN
+        assertThatThrownBy(() -> validator.validate(token))
+                // THEN
+                .isInstanceOf(InvalidTokenException.class)
+                .hasMessage("test");
     }
 }
