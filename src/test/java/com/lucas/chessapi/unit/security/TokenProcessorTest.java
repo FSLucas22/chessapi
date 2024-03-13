82203621079a804d7997e910bf5cc945ab4a3d6a
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-12 23:08:25 -0300

    refactor(JwtTokenProcessosTest): Simplify tests removing inheritance

--- src/test/java/com/lucas/chessapi/unit/security/TokenProcessorTest.java
+++ src/test/java/com/lucas/chessapi/unit/security/TokenProcessorTest.java
@@ -2,13 +2,18 @@ package com.lucas.chessapi.unit.security;
 
 import com.lucas.chessapi.builders.DateFactory;
 import com.lucas.chessapi.configuration.SecurityConfiguration;
-import com.lucas.chessapi.domain.security.ContextTokenProcessor;
 import com.lucas.chessapi.security.jwt.TokenProcessor;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TokenProcessorTest extends ContextTokenProcessor {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TokenProcessorTest {
+    protected SecurityConfiguration securityConfiguration;
+    protected TokenProcessor tokenProcessor;
+
     @BeforeEach
     void setUp() {
         securityConfiguration = new SecurityConfiguration();
@@ -22,21 +27,33 @@ public class TokenProcessorTest extends ContextTokenProcessor {
     void shouldIssueTokenWithCorrectExpirationDate() {
         var issueDate = DateFactory.today();
         var expirationDate = DateFactory.expirationDate(issueDate, securityConfiguration.getExpiration());
+        var token = tokenProcessor.issueToken("123", issueDate);
+        var expectedClaims = Jwts.claims()
+                .setSubject("123")
+                .setIssuedAt(issueDate)
+                .setExpiration(expirationDate);
 
-        givenSubject("123");
-        whenIssueTokenIsCalled(issueDate);
-        thenParsedClaimsFieldsShouldMatch(
-                Jwts.claims()
-                        .setSubject("123")
-                        .setIssuedAt(issueDate)
-                        .setExpiration(expirationDate)
-        );
+        Claims claims = Jwts.parser()
+                .setSigningKey(securityConfiguration.getKey())
+                .parseClaimsJws(token).getBody();
+
+        assertThat(claims.getSubject()).isEqualTo(expectedClaims.getSubject());
+        assertThat(claims.getIssuedAt()).isEqualTo(expectedClaims.getIssuedAt());
+        assertThat(claims.getExpiration()).isEqualTo(expectedClaims.getExpiration());
     }
 
     @Test
     void shouldRecoverDataFromToken() {
-        givenSubject("123");
-        whenIssueTokenIsCalled(DateFactory.today());
-        thenShouldRecoverDataFromToken();
+        var token = tokenProcessor.issueToken("123", DateFactory.today());
+        var dto = tokenProcessor.getJwtTokenDtoFromToken(token);
+        var expectedClaims = dto.toClaims();
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(securityConfiguration.getKey())
+                .parseClaimsJws(token).getBody();
+
+        assertThat(claims.getSubject()).isEqualTo(expectedClaims.getSubject());
+        assertThat(claims.getIssuedAt()).isEqualTo(expectedClaims.getIssuedAt());
+        assertThat(claims.getExpiration()).isEqualTo(expectedClaims.getExpiration());
     }
 }
