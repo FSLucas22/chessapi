98067bd3cc36893d39fbc6a6acdbf4355bf4fd48
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-13 00:11:12 -0300

    refactor(AuthServiceTest): Simplify tests removing inheritance

--- src/test/java/com/lucas/chessapi/unit/service/AuthServiceTest.java
+++ src/test/java/com/lucas/chessapi/unit/service/AuthServiceTest.java
@@ -1,19 +1,43 @@
 package com.lucas.chessapi.unit.service;
 
 import com.lucas.chessapi.builders.UserEntityBuilderExtension;
-import com.lucas.chessapi.domain.service.ContextAuthServiceTest;
 import com.lucas.chessapi.dto.request.AuthRequestDto;
 import com.lucas.chessapi.dto.response.AuthResponseDto;
 import com.lucas.chessapi.exceptions.UserAuthenticationException;
 import com.lucas.chessapi.model.UserEntity;
+import com.lucas.chessapi.repository.UserRepository;
+import com.lucas.chessapi.security.jwt.TokenProcessor;
+import com.lucas.chessapi.service.impl.AuthServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static com.lucas.chessapi.Utils.generateRandomString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
-public class AuthServiceTest extends ContextAuthServiceTest {
-    private AuthRequestDto request;
-    private UserEntity user;
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+    @Mock
+    PasswordEncoder encoder;
+    @Mock
+    UserRepository repository;
+    @Mock
+    TokenProcessor tokenProcessor;
+    @InjectMocks
+    AuthServiceImpl service;
+
+    AuthRequestDto request;
+    UserEntity user;
 
     @BeforeEach
     void setUp() {
@@ -31,32 +55,50 @@ public class AuthServiceTest extends ContextAuthServiceTest {
 
     @Test
     void shouldAuthenticateUserWithCorrectRequest() {
-        givenUserExists(user);
-        givenReturnedTokenIs("1234");
-        givenEncodedPasswordMatches("test123", user.getPassword());
-        whenAuthenticationHappensFor(request);
-        thenShouldConsultReposityWithEmail("test@email.com");
-        thenShouldIssueToken();
-        thenResponseShouldBe(new AuthResponseDto("testuser", "1234"));
+        // GIVEN
+        when(repository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(tokenProcessor.issueToken(anyString(), any())).thenReturn("1234");
+        when(encoder.matches(request.password(), user.getPassword())).thenReturn(true);
+
+        // WHEN
+        var response = service.authenticate(request);
+
+        // THEN
+        verify(repository, times(1)).findByEmail("test@email.com");
+        verify(tokenProcessor, times(1)).issueToken(anyString(), any());
+        assertThat(response)
+                .usingRecursiveComparison()
+                .isEqualTo(new AuthResponseDto("testuser", "1234"));
     }
 
     @Test
     void shouldThrowUserAuthenticationExceptionWhenUserIsNotFound() {
-        givenUserRepositoryReturnsEmpty();
-        whenAuthenticationHappensFor(request);
-        thenShouldConsultReposityWithEmail("test@email.com");
-        thenShouldThrow(UserAuthenticationException.class, "Invalid email or password");
-        thenShouldNotIssueToken();
+        // GIVEN
+        when(repository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        // WHEN
+        assertThatThrownBy(() -> service.authenticate(request))
+                // THEN
+                .isInstanceOf(UserAuthenticationException.class)
+                .hasMessage("Invalid email or password");
+
+        verify(repository, times(1)).findByEmail("test@email.com");
+        verify(tokenProcessor, never()).issueToken(anyString(), any());
     }
 
     @Test
     void shouldThrowUserAuthenticationExceptionWhenPasswordIsIncorrect() {
-        request = new AuthRequestDto("test@email.com", "test123");
-        givenUserExists(user);
-        givenEncodedPasswordDontMatch("test123", user.getPassword());
-        whenAuthenticationHappensFor(request);
-        thenShouldConsultReposityWithEmail("test@email.com");
-        thenShouldThrow(UserAuthenticationException.class, "Invalid email or password");
-        thenShouldNotIssueToken();
+        // GIVEN
+        when(repository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(encoder.matches(request.password(), user.getPassword())).thenReturn(false);
+
+        // WHEN
+        assertThatThrownBy(() -> service.authenticate(request))
+                // THEN
+                .isInstanceOf(UserAuthenticationException.class)
+                .hasMessage("Invalid email or password");
+
+        verify(repository, times(1)).findByEmail("test@email.com");
+        verify(tokenProcessor, never()).issueToken(anyString(), any());
     }
 }
