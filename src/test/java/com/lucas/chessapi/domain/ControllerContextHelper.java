48179a33de152b8d4f507559d7ff427e4b76bcc1
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-03-14 00:05:45 -0300

    refactor(GameControllerTest): Simplify tests by shortening inheritance chain

--- src/test/java/com/lucas/chessapi/domain/ControllerContextHelper.java
+++ src/test/java/com/lucas/chessapi/domain/ControllerContextHelper.java
@@ -1,16 +1,11 @@
 package com.lucas.chessapi.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lucas.chessapi.model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -18,40 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class ControllerContextHelper extends TestContextHelper {
+public class ControllerContextHelper {
     @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
     protected ObjectMapper mapper;
-
-    @Autowired
-    protected ContextAuthenticationHelper authContext;
-
-    protected ResultActions result;
-
-    protected void givenAuthenticationHappensFor(UserEntity user) {
-        authContext.givenAuthenticationHappensFor(user);
-    }
-
-    protected <T> void whenRequestIsPerformed(MockHttpServletRequestBuilder request, T content) {
-        try {
-            result = mockMvc.perform(request.content(mapper.writeValueAsString(content)));
-        } catch (Exception e) {
-            error = e;
-        }
-    }
-
-    protected void whenRequestIsPerformed(RequestBuilder request) {
-        try {
-            result = mockMvc.perform(request);
-        } catch (Exception e) {
-            error = e;
-        }
-    }
-
-    protected void thenIsExpectedFromResponse(ResultMatcher... resultMatchers) throws Exception {
-        result.andExpectAll(resultMatchers);
-    }
-
 }
--- src/test/java/com/lucas/chessapi/integration/controller/GameControllerTest.java
+++ src/test/java/com/lucas/chessapi/integration/controller/GameControllerTest.java
@@ -1,32 +1,54 @@
 package com.lucas.chessapi.integration.controller;
 
 import com.lucas.chessapi.builders.GameEntityBuilder;
-import com.lucas.chessapi.domain.controller.ContextGameControllerTest;
+import com.lucas.chessapi.domain.ControllerContextHelper;
 import com.lucas.chessapi.dto.request.CreateGameRequestDto;
+import com.lucas.chessapi.dto.response.CreateGameResponseDto;
 import com.lucas.chessapi.game.enums.OrderedPairCreationType;
 import com.lucas.chessapi.model.GameEntity;
 import com.lucas.chessapi.model.UserEntity;
+import com.lucas.chessapi.repository.GameRepository;
+import com.lucas.chessapi.repository.UserRepository;
+import com.lucas.chessapi.security.jwt.TokenProcessor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
+import static com.lucas.chessapi.builders.DateFactory.today;
 import static com.lucas.chessapi.builders.UserEntityBuilderExtension.user;
 import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class GameControllerTest extends ContextGameControllerTest {
-    private UserEntity alice;
-    private UserEntity bob;
+public class GameControllerTest extends ControllerContextHelper {
+    @Autowired
+    UserRepository userRepository;
 
-    private GameEntity game1;
-    private GameEntity game2;
+    @Autowired
+    GameRepository gameRepository;
+
+    @Autowired
+    TokenProcessor tokenProcessor;
+
+    @Value("${security.config.prefix}")
+    String prefix;
+
+    UserEntity alice;
+    UserEntity bob;
+
+    GameEntity game1;
+    GameEntity game2;
 
     @BeforeEach
     void setUp() {
-        authContext.clean();
         alice = user(null, "Alice");
         bob = user(null, "Bob");
 
@@ -41,90 +63,83 @@ public class GameControllerTest extends ContextGameControllerTest {
 
     @Test
     void shouldReturnStatus200WhenGetGameRequestIsValid() throws Exception {
-        givenAuthenticationHappensFor(alice);
-        whenGetGameRequestIsPerformedForId(game1.getId());
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(
-                status().isOk(),
-                jsonPath("$.id", equalTo(game1.getId().intValue())),
-                jsonPath("$.firstPlayer.id", equalTo(alice.getId().intValue())),
-                jsonPath("$.firstPlayer.username", equalTo(alice.getUsername())),
-                jsonPath("$.secondPlayer.id", equalTo(bob.getId().intValue())),
-                jsonPath("$.secondPlayer.username", equalTo(bob.getUsername()))
-        );
+        var header = generateAuthenticationFor(alice);
+        performGetGameRequestIsPerformedFor(game1.getId(), header)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.id", equalTo(game1.getId().intValue())),
+                        jsonPath("$.firstPlayer.id", equalTo(alice.getId().intValue())),
+                        jsonPath("$.firstPlayer.username", equalTo(alice.getUsername())),
+                        jsonPath("$.secondPlayer.id", equalTo(bob.getId().intValue())),
+                        jsonPath("$.secondPlayer.username", equalTo(bob.getUsername()))
+                );
     }
 
     @Test
     void shouldReturnStatus403WhenRequestIsPerformedWithoutAuthentication() throws Exception {
-        whenGetGameRequestIsPerformedForId(1L);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isForbidden());
+        performGetGameRequestIsPerformedFor(1L, "").andExpect(status().isForbidden());
     }
 
     @Test
     void shouldReturnStatus404WhenGameDontExist() throws Exception {
-        givenAuthenticationHappensFor(alice);
-        whenGetGameRequestIsPerformedForId(-1L);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isNotFound());
+        var header = generateAuthenticationFor(alice);
+        performGetGameRequestIsPerformedFor(-1L, header).andExpect(status().isNotFound());
     }
 
     @Test
     void shouldReturnStatus200WhenGetAllUserGamesIsCalledAndUserIsAuthenticated() throws Exception {
-        givenAuthenticationHappensFor(alice);
-        whenGetAllUserGamesIsRequestPerformedWithParams(alice.getId(), 0, 3);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(
-                status().isOk(),
-                jsonPath("$.player.id", equalTo(alice.getId().intValue())),
-                jsonPath("$.player.username", equalTo(alice.getUsername())),
-                jsonPath("$.games", hasSize(2)),
-                jsonPath("$.hasNext", equalTo(false))
-        );
-
-        thenIsExpectedFromResponse(gameInIndexMatches(0, game1));
-        thenIsExpectedFromResponse(gameInIndexMatches(1, game2));
+        var header = generateAuthenticationFor(alice);
+        performGetAllUserGamesIsRequestPerformedWithParams(alice.getId(), 0, 3, header)
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.player.id", equalTo(alice.getId().intValue())),
+                        jsonPath("$.player.username", equalTo(alice.getUsername())),
+                        jsonPath("$.games", hasSize(2)),
+                        jsonPath("$.hasNext", equalTo(false))
+                )
+                .andExpectAll(gameInIndexMatches(0, game1))
+                .andExpectAll(gameInIndexMatches(1, game2));
     }
 
     @Test
     void shouldReturnStatus409WhenGetAllUserGamesIsCalledAndUserDontExist() throws Exception {
-        givenAuthenticationHappensFor(alice);
-        whenGetAllUserGamesIsRequestPerformedWithParams(-1L, 0, 3);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isConflict());
+        var header = generateAuthenticationFor(alice);
+        performGetAllUserGamesIsRequestPerformedWithParams(-1L, 0, 3, header)
+                .andExpect(status().isConflict());
     }
 
     @Test
     void shouldReturnStatus403WhenGetAllUserGamesIsCalledAndUserIsNotAuthenticated() throws Exception {
-        whenGetAllUserGamesIsRequestPerformedWithParams(1L, 0, 3);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isForbidden());
+        performGetAllUserGamesIsRequestPerformedWithParams(1L, 0, 3, "")
+                .andExpect(status().isForbidden());
     }
 
     @Test
     void shouldReturnStatus201WhenCreateGameRequestIsCalledWithAuthenticatedUser() throws Exception {
         var request = new CreateGameRequestDto(bob.getId(), OrderedPairCreationType.AS_PASSED);
-        givenAuthenticationHappensFor(alice);
-        whenCreateGameRequestIsPerformedWith(request);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isCreated());
-        thenHeaderLocationFollowedByIdShouldBe("/chessapi/game/");
-        thenIsExpectedFromResponse(
+        var header = generateAuthenticationFor(alice);
+        var performResult = performCreateGameRequestFor(request, header);
+
+        var responseBody = performResult.andReturn().getResponse().getContentAsString();
+        var response = mapper.readValue(responseBody, CreateGameResponseDto.class);
+        var id = response.id();
+
+        performResult.andExpectAll(
+                status().isCreated(),
                 jsonPath("$.id", notNullValue()),
                 jsonPath("$.firstPlayer.id", equalTo(alice.getId().intValue())),
                 jsonPath("$.firstPlayer.username", equalTo(alice.getUsername())),
                 jsonPath("$.secondPlayer.id", equalTo(bob.getId().intValue())),
-                jsonPath("$.secondPlayer.username", equalTo(bob.getUsername()))
+                jsonPath("$.secondPlayer.username", equalTo(bob.getUsername())),
+                header().string("location", "/chessapi/game/" + id)
         );
     }
 
     @Test
     void shouldReturnStatus409WhenCreateGameRequestIsCalledWithNonExistingAdversary() throws Exception {
         var request = new CreateGameRequestDto(-1L, OrderedPairCreationType.AS_PASSED);
-        givenAuthenticationHappensFor(alice);
-        whenCreateGameRequestIsPerformedWith(request);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(
+        var header = generateAuthenticationFor(alice);
+        performCreateGameRequestFor(request, header).andExpectAll(
                 status().isConflict(),
                 jsonPath("$.errors", hasSize(1)),
                 jsonPath("$.errors[0]", equalTo("Player not found"))
@@ -134,10 +149,8 @@ public class GameControllerTest extends ContextGameControllerTest {
     @Test
     void shouldReturnStatus400WhenCreateGameRequestIsCalledWithInvalidRequest() throws Exception {
         var request = new CreateGameRequestDto(null, null);
-        givenAuthenticationHappensFor(alice);
-        whenCreateGameRequestIsPerformedWith(request);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(
+        var header = generateAuthenticationFor(alice);
+        performCreateGameRequestFor(request, header).andExpectAll(
                 status().isBadRequest(),
                 jsonPath("$.errors", hasSize(2)),
                 jsonPath("$.errors", containsInAnyOrder(
@@ -150,12 +163,10 @@ public class GameControllerTest extends ContextGameControllerTest {
     @Test
     void shouldReturnStatus403WhenCreateGameIsCalledWithoutAuthentication() throws Exception {
         var request = new CreateGameRequestDto(1L, OrderedPairCreationType.AS_PASSED);
-        whenCreateGameRequestIsPerformedWith(request);
-        thenShouldHaveNoErrors();
-        thenIsExpectedFromResponse(status().isForbidden());
+        performCreateGameRequestFor(request, "").andExpect(status().isForbidden());
     }
 
-    private ResultMatcher[] gameInIndexMatches(Integer index, GameEntity game) {
+    ResultMatcher[] gameInIndexMatches(Integer index, GameEntity game) {
         var field = String.format("$.games[%d]", index);
         var firstPlayer = game.getFirstPlayer();
         var secondPlayer = game.getSecondPlayer();
@@ -169,5 +180,40 @@ public class GameControllerTest extends ContextGameControllerTest {
                 jsonPath(field + ".createdAt", equalTo(game.getCreatedAt().format(formatter))),
                 jsonPath(field + ".updatedAt", equalTo(game.getUpdatedAt().format(formatter)))
         };
+    }
+
+    void given(UserEntity... entities) {
+        userRepository.deleteAll();
+        Arrays.stream(entities).forEach(userRepository::save);
+    }
+
+    void given(GameEntity... entities) {
+        Arrays.stream(entities).forEach(gameRepository::save);
+    }
+
+    ResultActions performGetGameRequestIsPerformedFor(Long id, String header) throws Exception {
+        return mockMvc.perform(get("/chessapi/game/" + id)
+                .header("Authorization", header));
+    }
+
+    ResultActions performGetAllUserGamesIsRequestPerformedWithParams(
+            Long id, Integer pageNumber, Integer pageSize, String header
+    ) throws Exception {
+        return mockMvc.perform(
+                get(String.format("/chessapi/game/user/%d?page=%d&limit=%d", id, pageNumber, pageSize))
+                        .header("Authorization", header));
+    }
+
+    ResultActions performCreateGameRequestFor(CreateGameRequestDto request, String header)
+            throws Exception {
+        return mockMvc.perform(post("/chessapi/game")
+                .header("Authorization", header)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)));
+    }
+
+    String generateAuthenticationFor(UserEntity user) {
+        var token = tokenProcessor.issueToken(user.getId().toString(), today());
+        return prefix + " " + token;
     }
 }
